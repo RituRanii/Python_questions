838534f0401eab62b5d02b031190c22e1594b42a
RituRanii <ritu199929@gmail.com>
2023-12-08 01:36:23 +0530

    Day 1

---
+++ binary_to_number.py
@@ -0,0 +1,15 @@
+def changeads(num):
+    b=bin(num)
+    b=b[2:len(b)]
+    b1=""
+    print(b)
+    for bit in b:
+        if bit=='1':
+            b1+='0'
+        else:
+            b1+='1'
+    sum1=0
+    for i in range(len(b1)):
+        sum1 = sum1+(int(b1[i])*(2**(len(b1)-(i+1))))
+    return sum1
+print(changeads(50))
